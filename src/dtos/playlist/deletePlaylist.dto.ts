54fff39ee214d03a6a539a6425999f04f0341933
RinoaYK <rinoayk@gmail.com>
2023-05-05 20:02:58 -0300

    Signup & Login

--- src/dtos/playlist/deletePlaylist.dto.ts
+++ src/dtos/playlist/deletePlaylist.dto.ts
@@ -10,4 +10,4 @@ export type DeletePlaylistOutputDTO = undefined
 export const DeletePlaylistSchema = z.object({
   token: z.string().min(1),
   idToDelete: z.string().min(1)
-})
+}).transform(data => data as DeletePlaylistInputDTO)
